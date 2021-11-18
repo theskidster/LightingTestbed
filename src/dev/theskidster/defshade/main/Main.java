615d5254b13b1460dd1825cfef6844bca3be8251
theskidster <theskidster@gmail.com>
2021-11-17 18:33:27 -0600

    added program entry point and dependencies

---
+++ src/dev/theskidster/defshade/main/Main.java
@@ -0,0 +1,17 @@
+package dev.theskidster.defshade.main;
+
+/**
+ * Nov 17, 2021
+ */
+
+/**
+ * @author J Hoffman
+ * @since  
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("asdf");
+    }
+    
+}
