d3801042d2a329ee4508123baf3d908b9c01daa8
theskidster <theskidster@gmail.com>
2021-12-01 19:14:51 -0600

    simplified blur fragment shader

--- src/dev/theskidster/light/shaders/blurFragment.glsl
+++ src/dev/theskidster/light/shaders/blurFragment.glsl
@@ -2,13 +2,13 @@
 
 in vec2 ioTexCoords;
 
-layout (location = 0) out vec4 ioFragColor;
-
 uniform float uWeight[5] = float[] (0.2270270270, 0.1945945946, 0.1216216216, 0.0540540541, 0.0162162162);
 uniform sampler2D uBloomTexture;
 
+out vec4 ioResult;
+
 void main() {
-    vec2 texOffset = 6.0 / textureSize(uBloomTexture, 0);
+    vec2 texOffset = 1.0 / textureSize(uBloomTexture, 0);
     vec3 result    = texture(uBloomTexture, ioTexCoords).rgb * uWeight[0];
 
     for(int i = 1; i < 5; ++i) {
@@ -16,10 +16,11 @@ void main() {
         result += texture(uBloomTexture, ioTexCoords - vec2(texOffset.x * i, 0.0)).rgb * uWeight[i];
     }
 
+    /*
     for(int i = 1; i < 5; ++i) {
         result += texture(uBloomTexture, ioTexCoords + vec2(0.0, texOffset.y * i)).rgb * uWeight[i];
         result += texture(uBloomTexture, ioTexCoords - vec2(0.0, texOffset.y * i)).rgb * uWeight[i];
-    }
+    }*/
 
-    ioFragColor = texture(uBloomTexture, ioTexCoords);
+    ioResult = vec4(result, 1.0);
 }
